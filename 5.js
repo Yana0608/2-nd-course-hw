aebc3ba1fbedf6f9a440094cfa40856fed0031d0
Frolova Yana <yana.zarubina0608@mail.ru>
2024-02-13 23:54:02 +0300

    сделала 4 задание

---
+++ 5.js
@@ -0,0 +1,41 @@
+//задание 1
+function num(a, b){
+   return a<b ? a:b; 
+}
+
+//Задание 2
+function check(num) {
+    if (num % 2 ===0) {
+        return "число четное"
+    } else {
+        "число нечетное"
+    }
+}
+
+//Задание 3
+function parameter(num) {
+    console.log(num*num);
+}
+
+function paramet(num) {
+    return num*num
+}
+
+//Задание 4
+function user() {
+    let age = prompt ("Сколько вам лет?")
+    if (age < 0 ) {
+        console.log("Вы ввели неправильное значение");
+    } else if( age>=12) {
+        console.log("Привет друг");
+    }
+    else {
+console.log("Добро пожаловать");
+    }
+}
+
+//Задание 5
+
+//Задание 6
+
+//Задание 7
