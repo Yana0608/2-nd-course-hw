306d07194636e90875da5023a0600c2440a3fa07
Frolova Yana <yana.zarubina0608@mail.ru>
2024-02-15 01:58:41 +0300

    сделала дз

--- 5.js
+++ 5.js
@@ -1,11 +1,11 @@
 //задание 1
-function num(a, b){
-   return a<b ? a:b; 
+function num(a, b) {
+    return a < b ? a : b;
 }
 
 //Задание 2
 function check(num) {
-    if (num % 2 ===0) {
+    if (num % 2 === 0) {
         return "число четное"
     } else {
         "число нечетное"
@@ -14,28 +14,79 @@ function check(num) {
 
 //Задание 3
 function parameter(num) {
-    console.log(num*num);
+    console.log(num * num);
 }
 
 function paramet(num) {
-    return num*num
+    return num * num
 }
 
 //Задание 4
 function user() {
-    let age = prompt ("Сколько вам лет?")
-    if (age < 0 ) {
+    let age = prompt("Сколько вам лет?")
+    if (age < 0) {
         console.log("Вы ввели неправильное значение");
-    } else if( age>=12) {
+    } else if (age >= 12) {
         console.log("Привет друг");
     }
     else {
-console.log("Добро пожаловать");
+        console.log("Добро пожаловать");
     }
 }
 
 //Задание 5
-
+function start(a, b) {
+    if (isNaN(a) || isNaN(b)) {
+        return 'Одно или оба значения не являются числом'
+    } else {
+        return a * b;
+    }
+}
 //Задание 6
+function cubeNumber() {
+    let num = prompt("Введите число");
+    num = Number(num);
+    if (isNaN(num)) {
+        return 'Переданный параметр не является числом';
+    } else {
+        return `${num} в кубе равняется ${num ** 3}`;
+    }
+    console.log(cubeNumber);
+}
 
 //Задание 7
+function getArea() {
+    return Math.PI * this.radius * this.radius;
+}
+
+function getPerimeter() {
+    return 2 * Math.PI * this.radius;
+}
+
+const circle1 = {
+    radius: 5,
+    getArea,
+    getPerimeter,
+};
+
+const circle2 = {
+    radius: 10,
+    getArea,
+    getPerimeter,
+};
+
+//Задание 8
+function getSeason(month) {
+    if (monthz >= 3 && month <= 5) {
+        return "Весна";
+    } else if (month >= 6 && month <= 8) {
+        return "Лето";
+    } else if (month >= 9 && month <= 11) {
+        return "Осень";
+    } else if (month == 12 || month == 1 || month == 2) {
+        return " Зима";
+    } else {
+        return "Неизвестное значение";
+    }
+}
+console.log(getSeason(prompt("Введите число:")));
