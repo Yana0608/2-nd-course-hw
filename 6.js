48b543fdf8de50c5cbf7c3e91ee65f8978e6b7a7
Frolova Yana <yana.zarubina0608@mail.ru>
2024-02-27 15:47:50 +0300

    Исправила имена

--- 6.js
+++ 6.js
@@ -8,23 +8,23 @@ for ( let value of numb) {
 }
 
 //Задание 2
-const arr = [1, 5, 4, 10, 0, 3];
-const input = arr.indexOf(4);
+const numbs = [1, 5, 4, 10, 0, 3];
+const input = numbs.indexOf(4);
 console.log(input);
 
 //Задание 3
-const input = [1, 3, 5, 10, 20];
-const arr = input.join(' ');
-console.log(arr);
+const numbers = [1, 3, 5, 10, 20];
+const result = input.join(' ');
+console.log(result);
 
 //Задание 4
-let arr = [];
+let user = [];
 for( let i = 0; i <3; i++) {
     let input = [];
     for (let j = 0; j < 3; j++) {
         input.push(1);
     }
-    arr.push(input);
+    user.push(input);
 }
 console.log(arr);
 
@@ -34,9 +34,9 @@ end.push(2, 2, 2);
 console.log(end);
 
 //Задание 6
-let arr =[9, 8, 7, 'a', 6, 5];
-arr.sort((a, b)=> a - b). pop();
-console.log(arr);
+let num =[9, 8, 7, 'a', 6, 5];
+num.sort((a, b)=> a - b). pop();
+console.log(num);
 
 //Задание 7
 const arr = [9, 8, 7, 6, 5];
@@ -48,9 +48,9 @@ if (arr.includes(guess)) {
 }
 
 //Задание 8
-const arr = 'abcdef';
-const input = arr.split('').reverse().join('');
-console.log(input);
+const string = 'abcdef';
+const reversedString = string.split('').reverse().join('');
+console.log(reversedString);
 
 //Задание 9
 const numbArr = [[1, 2, 3,],[4, 5, 6]];
@@ -58,9 +58,9 @@ const view = numbArr.flat();
 console.log(view);
 
 //Задание 10
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for ( let i =0; i <arr.length - 1; i++) {
-    console.log(arr[i] + arr[i + 1]);
+const nubersArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for ( let i =0; i <nubersArr.length - 1; i++) {
+    console.log(nubersArr[i] + nubersArr[i + 1]);
 }
 
 //Задание 11
